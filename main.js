265d95b68bd847aa7ac1c8d5561e85f64b37c96a
Vika <vika_kuklina2010@mail.ru>
2026-04-04 21:31:29 +0300

    feat(js): условия и логические проверки для задач

--- main.js
+++ main.js
@@ -7,3 +7,19 @@ let dif = a - b;
 
 
 console.log(sum, dif)
+
+let title = "";
+if (title === "") {
+    console.log("Название задач не указано");
+} else {
+    console.log("Задача:", title)
+}
+
+let tasks = 5;
+if (tasks === 0) {
+  console.log("Список пуст");
+} else if (tasks <= 3) {
+  console.log("Немного задач");
+} else {
+  console.log("Много задач");
+}
